98a0a19e6511e2a010b1d2b5d20264f69c755c2f
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-06-25 17:19:42 -0700

    Chore - lint & format

--- app.py
+++ app.py
@@ -7,7 +7,9 @@ parser.add_argument("--runbook", type=str, help="runbook(s) to execute")
 parser.add_argument("--task", type=str, help="task(s) to execute")
 
 output_group = parser.add_mutually_exclusive_group()
-output_group.add_argument("--verbose", action="store_true", help="enable verbose output")
+output_group.add_argument(
+    "--verbose", action="store_true", help="enable verbose output"
+)
 output_group.add_argument("--silent", action="store_true")
 
 args = parser.parse_args()
