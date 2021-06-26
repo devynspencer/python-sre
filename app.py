e6d03f8b6347a85666a5a38847aabacadfbbf875
Devyn Spencer <devynspencer@users.noreply.github.com>
2021-06-25 17:34:54 -0700

    Refactor argument parser to use subcommands

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ import argparse
 parser = argparse.ArgumentParser(
     prog="sre", description="Toolset for service reliability engineering tasks"
 )
-parser.add_argument("--runbook", type=str, help="runbook(s) to execute")
-parser.add_argument("--task", type=str, help="task(s) to execute")
+subparsers = parser.add_subparsers(title="subcommands", description="valid subcommands")
+
+runbook_parser = subparsers.add_parser('runbook')
+runbook_parser.add_argument("--name", type=str, help="runbook(s) to execute")
+
+task_parser = subparsers.add_parser('task')
+task_parser.add_argument("--name", type=str, help="task(s) to execute")
 
 output_group = parser.add_mutually_exclusive_group()
 output_group.add_argument(
@@ -14,8 +19,4 @@ output_group.add_argument("--silent", action="store_true")
 
 args = parser.parse_args()
 
-if args.runbook is not None:
-    print(args.runbook)
-
-if args.task is not None:
-    print(args.task)
+print(args)
